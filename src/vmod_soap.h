2160daa7cd53448899e17a44462707b22a025b00
Jonathan Huot <jonathan.huot@thomsonreuters.com>
2017-02-03 12:18:44 +0100

    Removed unused static variables

--- src/vmod_soap.h
+++ src/vmod_soap.h
@@ -83,35 +83,18 @@ struct _error_info;
 typedef struct sess_record {
     VRT_CTX;
     apr_pool_t* pool;
-    const char* uuid;
+    const char* action;
+    const char* action_namespace;
     struct _error_info *error_info;
-    apr_hash_t* aggregated_context;
 } sess_record;
 
 extern apr_pool_t *s_module_pool;
 extern apr_hash_t* s_module_storage;
 extern apr_thread_mutex_t* s_module_mutex;
 // Common HTTP Headers
-static const char szReutersUUID[] = "\014REUTERSUUID:";
-static const char szTransactionId[] = "\016TransactionId:";
-static const char szWSGGuid[] = "\013X-Wsg-Guid:";
-static const char szTornadoUUID[] = "\022X-TR-userIdentity:";
-static const char szTornadoTimings[] = "\024X-TR-tornadoTimings:";
-static const char szTornadoAppinfo[] = "\025X-TR-applicationInfo:";
-static const char szUUID[] = "\005UUID:";
 static const char szContentLength[] = "\017Content-Length:";
 static const char szContentEncoding[] = "\021Content-Encoding:";
 
-static const struct gethdr_s VGC_HDR_REQ_ReutersUUID = { HDR_REQ, szReutersUUID };
-static const struct gethdr_s VGC_HDR_REQ_TransactionId = { HDR_REQ, szTransactionId };
-static const struct gethdr_s VGC_HDR_RESP_WSGGuid = { HDR_RESP, szWSGGuid };
-static const struct gethdr_s VGC_HDR_BERESP_WSGGuid = { HDR_BERESP, szWSGGuid };
-static const struct gethdr_s VGC_HDR_REQ_TornadoUUID = { HDR_REQ, szTornadoUUID };
-static const struct gethdr_s VGC_HDR_REQ_TornadoTimings = { HDR_REQ, szTornadoTimings };
-static const struct gethdr_s VGC_HDR_RESP_TornadoTimings = { HDR_RESP, szTornadoTimings };
-static const struct gethdr_s VGC_HDR_BERESP_TornadoTimings = { HDR_BERESP, szTornadoTimings };
-static const struct gethdr_s VGC_HDR_REQ_TornadoAppinfo = { HDR_REQ, szTornadoAppinfo };
-static const struct gethdr_s VGC_HDR_REQ_UUID = { HDR_REQ, szUUID };
 static const struct gethdr_s VGC_HDR_REQ_Content_2d_Length = { HDR_REQ, szContentLength };
 static const struct gethdr_s VGC_HDR_BERESP_Content_2d_Length = { HDR_BERESP, szContentLength };
 
